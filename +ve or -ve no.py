f642361e1105584256e984c754621eb0d7cce56c
njsuriya <42180462+njsuriya@users.noreply.github.com>
2018-08-13 15:47:07 +0530

    Odd/Even and Positive/Negative
    Using Python 3.6

---
+++ +ve or -ve no.py
@@ -0,0 +1,8 @@
+a=int(input('Enter the Number:'))
+if a>0:
+ print('POSITIVE')
+elif a==0:
+ print('ZERO')
+else:
+ print('NEGATIVE')
+ 
---
+++ even or odd.py
@@ -0,0 +1,8 @@
+a=int(input("Enter the Number:"))
+if a>0:
+    if a%2:
+        print("ODD")
+    else:
+        print("EVEN")
+else:
+    print("INVALID")
