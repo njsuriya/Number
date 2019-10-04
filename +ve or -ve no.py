e1c0750bd52e6ee926c5d04533fd7fa3654407f8
PotatoK123 <56174807+PotatoK123@users.noreply.github.com>
2019-10-04 23:45:31 +0100

    Added fail-safe
    Added fail-safe

--- +ve or -ve no.py
+++ +ve or -ve no.py
@@ -1,8 +1,18 @@
-a=int(input('Enter the Number:'))
+while True:
+    
+ try:
+  a=int(input('Enter the Number:'))
+  break
+  
+ except:
+  print("Please enter a NUMBER!")
+
 if a>0:
  print('POSITIVE')
+ 
 elif a==0:
  print('ZERO')
+ 
 else:
  print('NEGATIVE')
  
